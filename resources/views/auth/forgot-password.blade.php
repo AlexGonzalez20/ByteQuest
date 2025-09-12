1efa849bfbcd15a289442e755f300100a8df7ad2
Luis <la.gonzalezd33@gmail.com>
2025-09-12 06:54:58 -0500

    Update forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title>Recuperar Contraseña</title>
     @vite('resources/css/forgot-reset.css')
 </head>
+
 <body>
+    <div class="floating-circle"></div>
+    <div class="floating-circle"></div>
+    <div class="floating-circle"></div>
+    <div class="floating-circle"></div>
+    <div class="floating-circle"></div>
     <div class="container">
         <h2>¿Olvidaste tu contraseña?</h2>
         <form method="POST" action="{{ route('password.email') }}">
@@ -14,10 +21,11 @@
             <input type="email" name="email" placeholder="Ingresa tu correo" required>
             <button type="submit">Enviar enlace de recuperación</button>
         </form>
-        @if(session('status'))
+        @if (session('status'))
             <p style="color: green;">{{ session('status') }}</p>
         @endif
         <a href="{{ route('login') }}">Volver al login</a>
     </div>
 </body>
+
 </html>
